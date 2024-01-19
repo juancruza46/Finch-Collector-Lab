73c5ae7bf6f34f4de0a055688dea6007110756e0
juancruza46 <juan.cruza46@gmail.com>
2024-01-18 21:42:58 -0500

    creates add finch form with functionality, (just have to add navigation)

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Finch
 
 # Create your testing finches here
@@ -20,6 +21,10 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', {'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
 
 
 
