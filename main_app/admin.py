b87be8e6d82ccd0a2cf5f5012d75ab3c30e9dcd1
juancruza46 <juan.cruza46@gmail.com>
2024-01-25 20:06:01 -0500

    currently only admin can see feedings added and saved successfully

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 #from finch model
-from.models import Finch
+from.models import Finch, Feeding
 
 #register models
 admin.site.register(Finch)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,30 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+MEALS = (
+    # this is a tuple with multiple tuples
+    # each of these is called a 2-tuple
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Feeding(models.Model):
+    # our model attributes go here
+    date = models.DateField()
+    # B-reakfast
+    # L-unch
+    # D-inner
+    meal = models.CharField(
+        max_length=1,
+        # add the custom 'choices' field option
+        # this is what will create our dropdown menu
+        choices=MEALS,
+        # set the default choice, to be 'B'
+        default=MEALS[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
